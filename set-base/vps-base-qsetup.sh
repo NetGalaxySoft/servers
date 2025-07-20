b1c3efb0a913055f27b6b4a35205bde84aa8c34b
Ilko Iordanov <95051040+NetGalaxySoft@users.noreply.github.com>
2025-07-20 13:05:00 +0000

    Update vps-base-qsetup.sh

--- set-base/vps-base-qsetup.sh
+++ set-base/vps-base-qsetup.sh
@@ -170,6 +170,7 @@ if sudo grep -q '^BASE_RESULT_MODULE1=' "$SETUP_ENV_FILE" 2>/dev/null; then
 else
   echo "BASE_RESULT_MODULE1=✅" | sudo tee -a "$SETUP_ENV_FILE" > /dev/null
 fi
+fi
 echo ""
 echo ""
 
