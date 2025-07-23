f43471a15c57df918fbb5aa6d6a8fa3202c15a94
Ilko Iordanov <95051040+NetGalaxySoft@users.noreply.github.com>
2025-07-23 09:23:26 +0000

    Update vps-dns-dnssec.sh

--- set-dns/vps-dns-dnssec.sh
+++ set-dns/vps-dns-dnssec.sh
@@ -194,13 +194,13 @@ for ZONE in "${ZONES[@]}"; do
   echo "🔐 Генериране на KSK и ZSK за $ZONE..."
 
   # KSK (Key Signing Key)
-  if ! sudo dnssec-keygen -K "$DNSSEC_DIR" -a RSASHA256 -b 2048 -n ZONE -f KSK "$ZONE"; then
+  if ! sudo -u bind dnssec-keygen -K "$DNSSEC_DIR" -a RSASHA256 -b 2048 -n ZONE -f KSK "$ZONE"; then
     echo "❌ Грешка при генериране на KSK за $ZONE!"
     exit 1
   fi
 
   # ZSK (Zone Signing Key)
-  if ! sudo dnssec-keygen -K "$DNSSEC_DIR" -a RSASHA256 -b 1024 -n ZONE "$ZONE"; then
+  if ! sudo -u bind dnssec-keygen -K "$DNSSEC_DIR" -a RSASHA256 -b 1024 -n ZONE "$ZONE"; then
     echo "❌ Грешка при генериране на ZSK за $ZONE!"
     exit 1
   fi
